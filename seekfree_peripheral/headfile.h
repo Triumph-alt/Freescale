1f21d02e982c77ac360b36cba5fc320b5907cfa7
Triumph-alt <150408992+Triumph-alt@users.noreply.github.com>
2025-02-18 20:48:50 +0800

    first
    空的工程

---
+++ seekfree_peripheral/headfile.h
@@ -0,0 +1,13 @@
+#ifndef __HEADFILE_H_
+#define __HEADFILE_H_
+
+
+#include "isr.h"
+
+
+
+
+
+
+
+#endif
---
+++ user/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void main(void)
+{
+	board_init();// 初始化寄存器,不能删
+	
+    while(1)
+	{
+		
+    }
+}
